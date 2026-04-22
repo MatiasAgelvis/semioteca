ef76dc5ad335ae79ae8aef6eec7db2bab9065e7d
Matias Agelvis <magelvisdoz@gmail.com>
2026-04-22 15:38:19 +0200

    feat(build): update vercel-build.sh and vercel-install.sh to use absolute paths for virtual environment

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-export PATH="./.vercel-venv/bin:$PATH"
+ROOT="$(pwd)"
+export PATH="$ROOT/.vercel-venv/bin:$PATH"
+export PYPANDOC_PANDOC="$ROOT/.vercel-venv/bin/pandoc"
 npm run build
--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -6,6 +6,13 @@ python3 -m venv .vercel-venv
 if [ ! -x ./.vercel-venv/bin/python3 ] && [ -x ./.vercel-venv/bin/python ]; then
   ln -sf python ./.vercel-venv/bin/python3
 fi
+mkdir -p ./.vercel-venv/bin
 ./.vercel-venv/bin/python -m pip install --upgrade pip
 ./.vercel-venv/bin/python -m pip install -r requirements.txt
-./.vercel-venv/bin/python -m pip install pandoc
+ROOT_PYTHON_PATH="$(pwd)/.vercel-venv/bin"
+./.vercel-venv/bin/python - <<PY
+import os
+import pypandoc
+os.makedirs(r'${ROOT_PYTHON_PATH}', exist_ok=True)
+pypandoc.download_pandoc(targetfolder=r'${ROOT_PYTHON_PATH}')
+PY
