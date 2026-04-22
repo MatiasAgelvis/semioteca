5173de96ef93e2ac9f1e987f2972ed2ac0e870c1
Matias Agelvis <magelvisdoz@gmail.com>
2026-04-22 15:32:56 +0200

    feat(build): update content generation script to change directory before execution

--- scripts/venv-python.sh
+++ scripts/venv-python.sh
@@ -6,6 +6,10 @@ if [ -x "./.vercel-venv/bin/python3" ]; then
   exec "./.vercel-venv/bin/python3" "$@"
 elif [ -x "./.vercel-venv/bin/python" ]; then
   exec "./.vercel-venv/bin/python" "$@"
+elif [ -x "../.vercel-venv/bin/python3" ]; then
+  exec "../.vercel-venv/bin/python3" "$@"
+elif [ -x "../.vercel-venv/bin/python" ]; then
+  exec "../.vercel-venv/bin/python" "$@"
 else
   exec python3 "$@"
 fi
