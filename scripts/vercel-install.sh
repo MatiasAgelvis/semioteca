c1aa4be9e9ff94a3ec2c132d9c8f8c5a5756e778
Matias Agelvis <magelvisdoz@gmail.com>
2026-04-22 15:25:40 +0200

    feat(build): add symlink creation for python3 in Vercel virtual environment setup

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -3,6 +3,9 @@ set -eu
 
 npm --prefix frontend ci
 python3 -m venv .vercel-venv
+if [ ! -x ./.vercel-venv/bin/python3 ] && [ -x ./.vercel-venv/bin/python ]; then
+  ln -sf python ./.vercel-venv/bin/python3
+fi
 ./.vercel-venv/bin/python -m pip install --upgrade pip
 ./.vercel-venv/bin/python -m pip install -r requirements.txt
 ./.vercel-venv/bin/python -m pip install pandoc
