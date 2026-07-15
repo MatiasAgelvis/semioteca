b42cea9b7686f9978fb3dc7550df9901763cedca
Matias Agelvis <magelvisdoz@gmail.com>
2026-07-15 22:03:51 +0200

    fix(cards): correct share link label to copy link

--- frontend/src/routes/cards/+page.svelte
+++ frontend/src/routes/cards/+page.svelte
@@ -554,7 +554,7 @@
         <button class="btn btn-ghost btn-xs" type="button" onclick={closeFullResultsMode}
           >Volver al modo libro</button
         >
-        <button class="btn btn-ghost btn-xs shrink-0 gap-1" type="button" onclick={copyShareUrl} aria-label="Compartir enlace">
+        <button class="btn btn-ghost btn-xs shrink-0 gap-1" type="button" onclick={copyShareUrl} aria-label="Copiar enlace">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -564,7 +564,7 @@
             <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.225a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
             <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.225a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
           </svg>
-          <span class="hidden md:inline">Compartir enlace</span>
+          <span class="hidden md:inline">Copiar enlace</span>
         </button>
       {:else}
         <span>{filteredCards.length} tarjetas en este libro.</span>
