845b31e065375ff44b4740c2e6e53835f85bd9b7
Matias Agelvis <magelvisdoz@gmail.com>
2026-09-10 16:54:35 +0200

    refactor(ui): replace inline SVG icons with VectorPolygon

--- frontend/src/routes/cards/[id]/+page.svelte
+++ frontend/src/routes/cards/[id]/+page.svelte
@@ -14,7 +14,7 @@
     buildCardFullText,
     copyTextToClipboard,
   } from '$lib/utils/citation';
-  import { LucideUmbrella } from '@lucide/svelte';
+  import { LucideUmbrella, VectorPolygon } from '@lucide/svelte';
 
   let { data }: { data: PageData } = $props();
 
@@ -89,28 +89,6 @@
           ← Volver al repositorio
         </a>
       {/if}
-      {#if data.relations.length > 0}
-        <button
-          type="button"
-          class="btn btn-ghost btn-xs md:btn-sm gap-1"
-          onclick={() => (sheetOpen = true)}
-          title="Tarjetas relacionadas"
-        >
-          <!-- TODO: Replace with icon from Lucide : vector-polygon -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-          >
-            <path
-              d="M418.4 157.9c35.3-8.3 61.6-40 61.6-77.9c0-44.2-35.8-80-80-80c-43.4 0-78.7 34.5-80 77.5L136.2 151.1C121.7 136.8 101.9 128 80 128c-44.2 0-80 35.8-80 80s35.8 80 80 80c12.2 0 23.8-2.7 34.1-7.6L259.7 407.8c-2.4 7.6-3.7 15.8-3.7 24.2c0 44.2 35.8 80 80 80s80-35.8 80-80c0-27.7-14-52.1-35.4-66.4l37.8-207.7zM156.3 232.2c2.2-6.9 3.5-14.2 3.7-21.7l183.8-73.5c3.6 3.5 7.4 6.7 11.6 9.5L317.6 354.1c-5.5 1.3-10.8 3.1-15.8 5.5L156.3 232.2z"
-            />
-          </svg>
-          <span>{data.relations.length}</span>
-        </button>
-      {/if}
     </div>
 
     <h1 class="text-3xl font-black lg:text-4xl truncate">{data.card.book}</h1>
@@ -150,19 +128,7 @@
           onclick={() => (sheetOpen = true)}
           title="Ver tarjetas relacionadas"
         >
-          <span aria-hidden="true">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-            >
-              <path
-                d="M418.4 157.9c35.3-8.3 61.6-40 61.6-77.9c0-44.2-35.8-80-80-80c-43.4 0-78.7 34.5-80 77.5L136.2 151.1C121.7 136.8 101.9 128 80 128c-44.2 0-80 35.8-80 80s35.8 80 80 80c12.2 0 23.8-2.7 34.1-7.6L259.7 407.8c-2.4 7.6-3.7 15.8-3.7 24.2c0 44.2 35.8 80 80 80s80-35.8 80-80c0-27.7-14-52.1-35.4-66.4l37.8-207.7zM156.3 232.2c2.2-6.9 3.5-14.2 3.7-21.7l183.8-73.5c3.6 3.5 7.4 6.7 11.6 9.5L317.6 354.1c-5.5 1.3-10.8 3.1-15.8 5.5L156.3 232.2z"
-              />
-            </svg>
-          </span>
+          <VectorPolygon size="1em" />
           <span class="hidden md:inline">Red</span>
         </button>
         <button
